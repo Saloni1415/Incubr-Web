297cb9f2b5fb88e9588bca375e06c206e5e845fd
Dhanavel <54678211+Dhanuvel163@users.noreply.github.com>
2021-07-14 21:18:04 +0530

    ALIGNMENT CHANGES IN LANDING PAGE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Scrollbar from 'smooth-scrollbar';
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'incubr';
+  ngOnInit(){
+
+  }
+  ngAfterViewInit(){
+    //FOR SCROLL BAR
+    //@ts-ignore
+    Scrollbar.init(document.querySelector('#my-scrollbar'));
+    // Scrollbar
+
+    $(document).on('mousemove', function(e:any){
+        $('#cursor').css({
+          left:  e.pageX,
+          top:   e.pageY
+        });
+    });
+  
+    $(window).on('scroll', function(e:any){
+      console.log('scrolled')
+        $('#cursor').css({
+          left:  e.pageX,
+          top:   e.pageY
+        });
+    });
+    //FOR CURSOR
+    // const cursor = document.querySelector('.cursor');
+    // document.addEventListener('mousemove', e => {
+    //   //@ts-ignore
+    //     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    // })
+    // document.addEventListener('click', () => {
+    //   //@ts-ignore
+    //     cursor.classList.add("expand");
+    //     setTimeout(() => {
+    //   //@ts-ignore
+    //         cursor.classList.remove("expand");
+    //     }, 500)
+    // })
+  }
 }
