a48a3640db470c38e3fbcb3b6594898afa65d3aa
Dhanavel <54678211+Dhanuvel163@users.noreply.github.com>
2021-07-30 10:27:27 +0530

    ALIGNMENT CHANGES

--- src/app/Components/section6/section6.component.ts
+++ src/app/Components/section6/section6.component.ts
@@ -11,5 +11,25 @@ export class Section6Component implements OnInit {
 
   ngOnInit(): void {
   }
+  ngAfterViewInit(){
+    let options = {
+      root: document.querySelector('#social '),
+      rootMargin: '0px',
+      threshold: 1
+    }
+    let observer = new IntersectionObserver((e:any)=>{
+      if(e[0].isIntersecting){
+        document.getElementById('whatsapp')?.setAttribute('src','../../../assets/incubrassets/wp.png')
+        document.getElementById('insta')?.setAttribute('src','../../../assets/incubrassets/insta.png')
+        document.getElementById('linkedin')?.setAttribute('src','../../../assets/incubrassets/linked.png')
+      }else{
+        document.getElementById('whatsapp')?.setAttribute('src','../../../assets/inc/whatsapp.png')
+        document.getElementById('insta')?.setAttribute('src','../../../assets/inc/insta.png')
+        document.getElementById('linkedin')?.setAttribute('src','../../../assets/inc/linked.png')
+      }
+    });
+    let target:any = document.querySelector('#redframe');
+    observer.observe(target);
+  }
 
 }
