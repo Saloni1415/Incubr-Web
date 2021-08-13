c252194a4dd6c15f9d0c9c066556880059723721
Dhanuvel163 <dhanuram99@gmail.com>
2021-08-14 01:43:43 +0530

    askhibi

--- src/app/Components/section3/section3.component.ts
+++ src/app/Components/section3/section3.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section3.component.scss']
 })
 export class Section3Component implements OnInit {
+  testimonials = [
+    {text:'The team is dedicated to ensuring a top tier experience, that all our needs are met. We’re very grateful to be working with Team Incubr!',client:'Alicia Lahey'},
+    {text:'Gopal and I have worked for over 7 years. If there is one characteristic that stands above them all is this trust we established. Gopal would give us direct feedback, complete a task we asked diligently, and represent the values and purpose of Amy’s on every interaction he had on our behalf.',client:'Paul Scheifer'},
+    {text:'Gopal‘s resilience and persistence have led to results beyond expectations and I am proud of what he has achieved.',client:'Damien Threadgold'},
+    {text:'His sense of problem-solving and willingness to go the extra mile always helps in creating more values for a client.',client:'Judhajit Bal'},
+  ]
+
   slideConfig = {
     "slidesToShow": 1, 
     "slidesToScroll": 1,
