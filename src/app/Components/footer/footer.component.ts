f22c1359d3bf38864bf1c999d9430646cd23921f
Dhanuvel163 <dhanuram99@gmail.com>
2021-08-25 21:39:02 +0530

    sd

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/data.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public data:DataService) { }
 
   ngOnInit(): void {
   }
-
+  navigate(item:any){
+    if(item=='Home'){
+    }else if(item=='Clients'){
+      setTimeout(()=>{
+        document.getElementById('clients')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Our Work'){
+      setTimeout(()=>{
+        document.getElementById('ourwork')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Incubr impact'){
+      setTimeout(()=>{
+        document.getElementById('impact')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Startups'){
+      setTimeout(()=>{
+        document.getElementById('startup')?.scrollIntoView()
+        setTimeout(()=>{
+          document.getElementById('startupopen')?.click()
+        },500)
+      },200)
+    }
+    else if(item=='Corporations'){
+      setTimeout(()=>{
+        document.getElementById('startup')?.scrollIntoView()
+        setTimeout(()=>{
+          document.getElementById('coropen')?.click()
+        },500)
+      },200)
+    }
+    else if(item=='Blogs'){
+      setTimeout(()=>{
+        document.getElementById('blogs')?.scrollIntoView()
+      },200)
+    }
+  }
 }
--- src/app/Pages/menu/menu.component.ts
+++ src/app/Pages/menu/menu.component.ts
@@ -8,8 +8,47 @@ import { DataService } from 'src/app/data.service';
 export class MenuComponent implements OnInit {
 
   constructor(public data:DataService) { }
-  items = ['home','startups','corporations','incubr diary','about','our impact']
+  items = ['Home','Clients','Startups','Corporations','Our Work','Incubr impact']
   ngOnInit(): void {
   }
-
+  navigate(item:any){
+    if(item=='Home'){
+      this.data.toggleMenu()
+    }else if(item=='Clients'){
+      this.data.toggleMenu()
+      setTimeout(()=>{
+        document.getElementById('clients')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Our Work'){
+      this.data.toggleMenu()
+      setTimeout(()=>{
+        document.getElementById('ourwork')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Incubr impact'){
+      this.data.toggleMenu()
+      setTimeout(()=>{
+        document.getElementById('impact')?.scrollIntoView()
+      },200)
+    }
+    else if(item=='Startups'){
+      this.data.toggleMenu()
+      setTimeout(()=>{
+        document.getElementById('startup')?.scrollIntoView()
+        setTimeout(()=>{
+          document.getElementById('startupopen')?.click()
+        },500)
+      },200)
+    }
+    else if(item=='Corporations'){
+      this.data.toggleMenu()
+      setTimeout(()=>{
+        document.getElementById('startup')?.scrollIntoView()
+        setTimeout(()=>{
+          document.getElementById('coropen')?.click()
+        },500)
+      },200)
+    }
+  }
 }
